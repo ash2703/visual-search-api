4c66be14fe7f410ea77e2ecebea0416bb81efcfa
ash2703 <nahtnam2703@gmail.com>
2021-03-15 00:43:33 +0530

    nothing changed

--- src/test.py
+++ src/test.py
@@ -4,7 +4,7 @@ class TestApp(unittest.TestCase):
     """Initial unit test for learning"""
 
     def test_return_backwards_string(self):
-        """Test return backwards simple string"""
+        """Fake testing"""
         random_string = "This is my test string"
         self.assertEqual(random_string, random_string)
 
