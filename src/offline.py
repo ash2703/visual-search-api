322bd374fc4adb5084552b752d420e8e39ab63b7
ash2703 <nahtnam2703@gmail.com>
2021-03-15 23:40:42 +0530

    fix consider all images in the folder for creating database

--- src/offline.py
+++ src/offline.py
@@ -3,7 +3,7 @@ import numpy as np
 from pathlib import Path
 from sklearn.metrics.pairwise import cosine_similarity,cosine_distances
 from tqdm import tqdm
-from src.extractor import FeatureExtractor
+from src.features import FeatureExtractor
 
 IMG_PATH = Path("./static/imgs")
 FEATURE_PATH = Path("./static/features")
@@ -11,7 +11,7 @@ FEATURE_PATH = Path("./static/features")
 
 if __name__ == "__main__":
     feats = FeatureExtractor()
-    for img_path in tqdm(sorted(IMG_PATH.glob("*.j*"))):
+    for img_path in tqdm(sorted(IMG_PATH.glob("*.*"))):
         feature_path = FEATURE_PATH / (img_path.stem + ".npy")
         if not feature_path.is_file():
             img_feats = feats.extract(img_path)
