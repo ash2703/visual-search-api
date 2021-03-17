8dd8aab0284450bd327f24ab5dc58be782893be0
ash2703 <nahtnam2703@gmail.com>
2021-03-17 11:54:04 +0530

    refactor feature database from config

--- src/offline.py
+++ src/offline.py
@@ -1,20 +1,25 @@
-from PIL import Image
-import numpy as np
+import os
 from pathlib import Path
+from PIL import Image
+
+import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity,cosine_distances
 from tqdm import tqdm
-from src.features import FeatureExtractor
 
-IMG_PATH = Path("./static/imgs")
-FEATURE_PATH = Path("./static/features")
+from src.features import FeatureExtractor
+from src.configs import config
 
 
 if __name__ == "__main__":
+    os.makedirs(config.FEATURE_PATH, exist_ok = True)
     feats = FeatureExtractor()
-    for img_path in tqdm(sorted(IMG_PATH.glob("*.*"))):
-        feature_path = FEATURE_PATH / (img_path.stem + ".npy")
-        if not feature_path.is_file():
-            img_feats = feats.extract(img_path)
-            np.save(feature_path, img_feats)
+    for img_path in tqdm(sorted(config.IMG_PATH.glob("**/*"))):
+        if img_path.suffix in config.IMAGE_EXTS:
+            feature_path = config.FEATURE_PATH / (img_path.stem + ".npy")
+            if not feature_path.is_file():
+                img_feats = feats.extract(img_path)
+                np.save(feature_path, img_feats)
+
+
 
     
